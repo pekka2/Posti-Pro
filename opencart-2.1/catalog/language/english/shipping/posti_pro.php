b943731f3a488db2e3e31b40ced26f0e75381928
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 17:48:33 +0300

    Create posti_pro.php

---
+++ opencart-2.1/catalog/language/english/shipping/posti_pro.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_title']                      = 'Posti';
+$_['text_free_cargo']                 = 'Rahtivapaasti:';
+$_['text_to_world']                   = 'Postal packages to';
+$_['text_kotijakelu']                 = 'Postin kotiinkuljetus';
+
+// error
+$_['text_curl_error']                   = 'Posti Pro Curl  Error';
+
+?>
