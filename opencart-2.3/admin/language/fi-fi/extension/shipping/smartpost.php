7415adbf89949461c0cf0847cc4bb075fcae7101
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 17:55:24 +0300

    Add files via upload

---
+++ opencart-2.3/admin/language/fi-fi/extension/shipping/smartpost.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']    = 'Posti Smartpost';
+
+// Text
+$_['text_extension']   = 'Laajennukset';
+$_['text_success']     = 'Olet muokannut Posti toimitustapaa onnistuneesti!';
+$_['text_edit']        = 'Muokkaa Postin smartpost toimitustapaa';
+
+// Tab
+$_['tab_additional_service']   = 'Lisäpalvelut';
+$_['tab_hinnasto']             = 'Hinnasto';
+
+// Column
+$_['column_kg']                = 'Paino';
+$_['column_price']             = 'Hinta';
+
+// Button
+$_['button_weight_add']        = 'Lisää hintaraja';
+
+// Entry
+$_['entry_tax_class']                = 'Veroluokka';
+$_['entry_length_class']             = 'Pituusluokka';
+$_['entry_weight_class']             = 'Painoluokka';
+$_['entry_geo_zone']                 = 'Alue';
+$_['entry_status']                   = 'Tila';
+$_['entry_sort_order']               = 'Järjestys';
+$_['entry_top']                      = 'Hakutuloksia kassalla';
+$_['entry_free_cargo']               = 'Rahtivapaa';
+$_['entry_cargo_sum']                = 'Rahtivapaus/<br>Rahtialennus raja/€';
+$_['entry_discount_cargo']           = 'Alennus rahdista  kotimaan myynnissä';
+$_['entry_discount_cargo_percent']   = 'Rahdin alennus prosentti';
+
+// Error
+$_['error_permission']      = 'Varoitus: Sinulla ei ole oikeuksia muokata Posti- toimitustapaa!';
+$_['error_warning']          = 'Virhe! Tarkista lomake ole hyvä.';
+$_['error_decimale_point']  = 'Virhe: Hinnastossa desimaali-erotttimena tulee olla piste(.)';
