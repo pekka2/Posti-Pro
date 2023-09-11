8dd34f47569537db6c0899cde01a77a9b1558464
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 16:15:40 +0300

    Create posti_pro.php

---
+++ opencart-1.5.5-1.5.6.4/admin/language/finnish/shipping/posti_pro.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']    = 'Posti Pro';
+
+// Text
+$_['text_extension']   = 'Laajennukset';
+$_['text_success']     = 'Olet muokannut Posti toimitustapaa onnistuneesti!';
+$_['text_edit']        = 'Muokkaa Psoti toimitustapaa';
+$_['text_hinnasto_2_kuvaus']   = 'Hinnaston kohteena Ruotsi, Tanska, Belgia, Alankomaat, Saksa, Luxemburg ja Baltian maat';
+$_['text_hinnasto_3_kuvaus']  = 'Hinnaston kohteena muut EU-maat, jotka eivät olleet Maksuvyöhykkeessä 1';
+$_['text_hinnasto_4_kuvaus']   = 'Hinnaston kohteena EU:n lähimmät kauppakumppanit kuten Yhdysvallat, Kanada, Sveitsi, Norja, Venäjä, Serbia, Turkki sekä Lähi-idän maat.';
+$_['text_hinnasto_5_kuvaus']   = 'Hinnaston kohteena maat, jotka eivät ole lähellä EU:ta manntieteellisesti tai talous-poliittisesti, kuten esim. Kiina, Japani, Afrikan ja Latinalisen Amerikan maat.';
+
+// Tab
+$_['tab_additional_service']   = 'Lisäpalvelut';
+$_['tab_hinnasto']             = 'Kotimaan hinnasto';
+$_['tab_hinnasto_2']           = 'Maksuvyöhyke 1';
+$_['tab_hinnasto_3']           = 'Maksuvyöhyke 2';
+$_['tab_hinnasto_4']           = 'Maksuvyöhyke 3';
+$_['tab_hinnasto_5']           = 'Maksuvyöhyke 4';
+
+// Column
+$_['column_kg']                = 'Paino';
+$_['column_price']             = 'Hinta';
+
+// Button
+$_['button_weight_add']        = 'Lisää hintaraja';
+
+// Entry
+$_['entry_kotijakelu_price']   = 'Kotijakelun lisähinta';
+$_['entry_kotijakelu']         = 'Kotijakelu';
+$_['entry_tax_class']                          = 'Veroluokka';
+$_['entry_length_class']                       = 'Pituusluokka';
+$_['entry_weight_class']                       = 'Painoluokka';
+$_['entry_geo_zone']                           = 'Alue';
+$_['entry_status']                             = 'Tila';
+$_['entry_free']                               = 'Ilmainen toimitus';
+$_['entry_sort_order']                         = 'Järjestys';
+$_['entry_top']                                = 'Hakutuloksia kassalla';
+$_['entry_free_cargo']                         = 'Rahtivapaa';
+$_['entry_cargo_sum']                          = 'Rahtivapaus/<br>Rahtialennus raja kotimaan myynnissä/€';
+$_['entry_cargo_foreign_countries_sum']        = 'Rahtivapaus/<br>Rahtialennus raja ulkomaan myynnissä/€';
+$_['entry_discount_cargo']                     = 'Alennus rahdista  kotimaan myynnissä';
+$_['entry_discount_cargo_percent']             = 'Rahdin alennus prosentti kotimaan myynnissä';
+$_['entry_discount_foreign_countries']         = 'Alennus rahdista ulkomaan myynnissä';
+$_['entry_discount_foreign_countries_percent'] = 'Rahdin alennus prosentti ulkomaan myynnissä';
+
+// Error
+$_['error_permission']       = 'Varoitus: Sinulla ei ole oikeuksia muokata Posti Pro- toimitustapaa!';
+$_['error_warning']          = 'Virhe! Tarkista lomake ole hyvä.';
+$_['error_decimale_point']   = 'Virhe: Hinnastossa desimaali-erotttimena tulee olla piste(.)';
+?>
