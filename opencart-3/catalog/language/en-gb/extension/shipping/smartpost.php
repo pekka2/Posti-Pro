14d912fa3375706534451a8221f45894260bd9e1
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-12 14:07:32 +0300

    Add files via upload

---
+++ opencart-3/catalog/language/en-gb/extension/shipping/smartpost.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_title']                      = 'Posti Smartpost';
+$_['text_free_cargo']                 = 'Rahtivapaasti:';
+$_['text_discount']                   = 'Alennus';
+
+
+// error
+$_['text_curl_error']                 = 'Smartpost Error';
